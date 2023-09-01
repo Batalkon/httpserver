f1509f22bfe799b6dda76a412a53940bd57f7f1f
Batalkon <108395903+Batalkon@users.noreply.github.com>
2023-09-01 17:28:58 +0300

    Add files via upload

--- httpserver/app/app.go
+++ httpserver/app/app.go
@@ -1,31 +1,57 @@
-package app
+package main
 
 import (
+	"context"
+	"crypto/tls"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/go-redis/redis/v8"
 )
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
-}
-func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
-	io.WriteString(w, "Hello, HTTP!\n")
-}
 func main() {
+	// Создание конфигурации Redis
+	redisConf := &redis.Options{
+		Addr:     "localhost:8080",
+		Password: "1234",
+		TLSConfig: &tls.Config{
+			InsecureSkipVerify: true, // Установите false, если у вас есть подлинный сертификат
+		},
+	}
+
+	// Создание клиента Redis
+	redisClient := redis.NewClient(redisConf)
+
+	// Проверка соединения
+	_, err := redisClient.Ping(context.Background()).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to Redis")
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", getRoot)
 	mux.HandleFunc("/hello", getHello)
 
-	err := http.ListenAndServe(":3333", mux)
+	err = http.ListenAndServe(":3333", mux)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
 	} else if err != nil {
 		fmt.Printf("error starting server: %s\n", err)
 		os.Exit(1)
 	}
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
 }
--- httpserver/go.mod
+++ httpserver/go.mod
@@ -1,9 +1,10 @@
-
 module github.com/batalkon/httpserver
 
-go 1.16
+go 1.21
+
+require github.com/go-redis/redis/v8 v8.11.5
 
 require (
-github.com/gin-gonic/gin v1.7.2
-github.com/go-redis/redis/v8 v8.12.0
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 )
